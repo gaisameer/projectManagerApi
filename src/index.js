8a1f3633212fb01c0ab852a99d69fef8f2b0f388
anonymousdath <anildath258@gmail.com>
2020-08-01 00:47:58 +0530

    testing commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-const express = require('express')
-const studRouter = require('./routers/studentRouter')
-const guideRouter = require('./routers/guideRouter')
-const projRouter = require('./routers/projectRouter')
-const logRouter = require('./routers/logRouter')
+const express = require("express");
+const studRouter = require("./routers/studentRouter");
+const guideRouter = require("./routers/guideRouter");
+const projRouter = require("./routers/projectRouter");
+const logRouter = require("./routers/logRouter");
 
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(studRouter);
+app.use(guideRouter);
+app.use(projRouter);
+app.use(logRouter);
 
+app.get("/", (req, res) => {
+  res.send("home");
+});
 
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-app.use(studRouter)
-app.use(guideRouter)
-app.use(projRouter)
-app.use(logRouter)
+app.listen(port, () => {
+  console.log("server running on " + port);
+});
 
-
-app.get('/',(req,res)=>{
-    res.send('home')
-})
-
-
-
-app.listen(port,()=>{
-    console.log('server running on ' + port )
-})
+//commit testing
